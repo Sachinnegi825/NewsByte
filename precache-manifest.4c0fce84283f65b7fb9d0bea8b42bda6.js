5bda065f03b2b4db40cd380351d28bd8388a134b
SACHIN NEGI <sachinnegi825@gmail.com>
2022-11-07 22:45:47 +0530

    Updates

---
+++ precache-manifest.4c0fce84283f65b7fb9d0bea8b42bda6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "89e1d120eeda162d501e",
+    "url": "/NewsByte/static/css/main.ddab3e2c.chunk.css"
+  },
+  {
+    "revision": "89e1d120eeda162d501e",
+    "url": "/NewsByte/static/js/main.89e1d120.chunk.js"
+  },
+  {
+    "revision": "dc4cc1735bfd4abc1f5d",
+    "url": "/NewsByte/static/js/1.dc4cc173.chunk.js"
+  },
+  {
+    "revision": "545fd4ffdae614f25a2a",
+    "url": "/NewsByte/static/js/runtime~main.545fd4ff.js"
+  },
+  {
+    "revision": "fe4d4044e83928c4b2c1ed2ca39113fc",
+    "url": "/NewsByte/index.html"
+  }
+];
